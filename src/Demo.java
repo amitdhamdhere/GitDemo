b558165cf4ea61254a15168a7a7cfc9a901f554f
amitdhamdhere <amit.dhamdhere@outlook.in>
2024-06-01 07:29:42 +0530

    This commit from Project A. After creating branch

--- src/Demo.java
+++ src/Demo.java
@@ -7,6 +7,7 @@ public class Demo {
 		System.out.println("This line is from Project B");
 		System.out.println("This line is from Project A");
 		System.out.println("This line is from Project B.Made chnages after creating develop branch");
+		System.out.println("This line is from Project A.Made chnages after creating develop branch");
 	}
 
 }
